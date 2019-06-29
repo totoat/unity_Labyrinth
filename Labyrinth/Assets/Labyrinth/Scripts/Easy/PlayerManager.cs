bb76717c77684882504dee12f4e32bf485a7adaf
totoat <33259052+totoat@users.noreply.github.com>
2019-06-30 08:49:43 +0900

    ReCommit

--- Labyrinth/Assets/Labyrinth/Scripts/Easy/PlayerManager.cs
+++ Labyrinth/Assets/Labyrinth/Scripts/Easy/PlayerManager.cs
@@ -4,21 +4,48 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour {
 
-    private new Rigidbody rigidbody;
     [SerializeField] AudioClip don;
+
+    private new Rigidbody rigidbody;
+    private new Renderer renderer;
+    private Color playerCol;
+
     AudioSource audioSource;
 
-
-	void Start () {
+    void Start () {
         rigidbody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
+        renderer = GetComponent<Renderer>();
+        playerCol= GetComponent<Renderer>().material.color;
+        
 	}
 
-	void Update () {
+	void Update ()
+    {
         // すり抜け防止策
-        transform.Rotate(1,0,1);
-        rigidbody.AddForce(new Vector3(0,-10,0));
-	}
+        transform.Rotate(1, 0, 1);
+        rigidbody.AddForce(new Vector3(0, -10, 0));
+
+        //ブレーキ機能
+        PlayerBrake();
+
+    }
+
+    private void PlayerBrake()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            renderer.material.color = Color.red;
+            rigidbody.isKinematic = true;
+
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space) || Input.GetMouseButtonUp(0))
+        {
+            renderer.material.color = playerCol;
+            rigidbody.isKinematic = false;
+        }
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
---
+++ Labyrinth/Assets/Labyrinth/Scripts/Hard/GoalHard.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GoalHard : MonoBehaviour
+{
+
+    [SerializeField] GameObject GoalUI;
+
+    AudioSource audioSource;
+    [SerializeField] AudioClip clearFanfare;
+    private bool alreadyGoal;
+
+    void Start()
+    {
+        GoalUI.SetActive(false);
+        alreadyGoal = false;
+
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // ゴール時の処理
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag=="Player")
+        {
+            if (!alreadyGoal)
+            {
+                GoalUI.SetActive(true);
+
+                audioSource.PlayOneShot(clearFanfare);
+
+                alreadyGoal = !alreadyGoal;
+            }
+
+        }
+
+    }
+}
